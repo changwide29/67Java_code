2165aa76b7e0bb453b4324d465ac47c2212ffe26
changwide29 <94587848+changwide29@users.noreply.github.com>
2024-08-05 13:35:04 +0700

    Add files via upload

---
+++ W08_03_VariableScope.java
@@ -0,0 +1,45 @@
+package W08;
+public class W08_03_VariableScope {
+
+    // Static Variable (ตัวแปรระดับคลาสที่เป็น static สามารถเข้าถึงได้โดยตรง)
+    static int x = 50;
+
+    // Instance Variables (คล้ายตัวแปรระดับคลาส แต่ต้องสร้าง instance ของคลาสเพื่อเข้าถึง)
+    int a = 100;
+    int b = 200;
+
+    public static void main(String[] args) {
+        
+        System.out.println("x = " + x);  // เข้าถึง static variable โดยตรง
+        
+        // สร้าง instance ของคลาสเพื่อเข้าถึง instance variables
+        W08_03_VariableScope obj = new W08_03_VariableScope();
+
+        // การเข้าถึง instance variables ผ่าน object
+        System.out.println("a before if = " + obj.a);
+        System.out.println("b before if = " + obj.b);
+
+        int y = 300; // Local Variable วงจรชีวิตของตัวแปร y จะทำงานเฉพาะใน main method
+        System.out.println("y in method = " + y);
+
+        if(true) {            
+            int c = 300;  // Local Variable วงจรชีวิตของตัวแปร c จะทำงานเฉพาะในบล็อกนี้เท่านั้น
+            System.out.println("y in if = " + y);
+            System.out.println("a in if = " + obj.a);
+            System.out.println("x in if = " + x);
+
+
+        }
+        System.out.println("y in method = " + y);
+        System.out.println("a after if = " + obj.a);
+    }
+}
+
+// javac .\W08\W08_03_VariableScope.java && java W08.W08_03_VariableScope 
+// ประกาศตัวแปร
+// สรุป
+// ตัวแปร Instance Variable และ Static Variable เป็นตัวแปรที่ถูกประกาศใช้ในระดับคลาส
+// ตัวแปร Local Variable เป็นตัวแปรที่ถูกประกาศภายในบล็อกของโค้ด เช่น ภายในฟังก์ชัน เมธอด หรือบล็อกของเงื่อนไข
+// ตัวแปร Local Variable จะมีวงจรชีวิตเฉพาะในบล็อกที่ถูกประกาศเท่านั้น
+// ตัวแปร Instance Variable และ Static Variable จะมีวงจรชีวิตตามวงจรชีวิตของ object หรือคลาสตามลำดับ
+// ตัวแปร Local Variable จะไม่สามารถเข้าถึงได้จากภายนอกบล็อกที่ถูกประกาศ
